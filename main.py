98ac9df5da11f91f3852ca2fe974f9e6ed950aad
JET\jonas <jonas.otto@field.io>
2024-12-23 19:29:37 +0100

    adding keypress test

--- main.py
+++ main.py
@@ -3,21 +3,33 @@ from libcamera import Transform, controls
 from pynput import keyboard
 import time
 
-picam2 = Picamera2()
-camera_config = picam2.create_preview_configuration()
-picam2.configure(camera_config)
+
+cam = Picamera2()
+loop = True
 
 def on_press(key):
     if key == keyboard.Key.up:
         pass
     elif key == keyboard.Key.down:
         pass
+    elif key == 's':
+        filename = strftime("%Y%m%d-%H%M%S") + '.png'
+        cam.capture_file(filename, format="png", wait=None)
+        print(f"\rCaptured {filename} succesfully")
+    elif key == "q":
+        loop = False
 
 listener = keyboard.Listener(on_press=on_press)
 listener.start()
 
-picam2.start_preview(Preview.QTGL, x=100, y=200, width=800, height=600)
-picam2.start()
-picam2.title_fields = ["ExposureTime", "AnalogueGain"]
+cam.start_preview(Preview.QTGL, x=100, y=200, width=800, height=600)
+cam.start()
+cam.title_fields = ["ExposureTime", "AnalogueGain"]
 
-time.sleep(360)
+try:
+    while True:
+        pass
+finally:
+    cam.stop_preview()
+    cam.stop()
+    cam.close()

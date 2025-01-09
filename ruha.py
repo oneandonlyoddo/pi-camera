c8daafe3476201f6112a851b548b44ad63a949b2
SILVER\jonas <jonas.otto@field.io>
2025-01-09 15:34:39 +0100

    fixing syntax

--- ruha.py
+++ ruha.py
@@ -1,15 +1,16 @@
 from gpiozero import Button
 from picamera2 import Picamera2
-import time
+from time import strftime
 
 button = Button(26)
 
 with Picamera2() as camera:
     camera.resolution = (4056, 3040)
     camera.framerate = 5
-    frame = int(time.time())
+    
     camera.start_preview()
     while True:
         button.wait_for_press()
-        camera.capture('/home/pi/Pictures/%03d.jpg' % frame)
-        frame += 1
+        filename = strftime("%Y%m%d-%H%M%S") + '.png'
+        camera.capture_file(filename, format="png", wait=None)
+        print(f"\rCaptured {filename} succesfully")

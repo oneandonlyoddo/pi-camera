cc5aeb4b75c04c56ae90d604ebd48e39331eac91
SILVER\jonas <jonas.otto@field.io>
2025-01-09 16:14:35 +0100

    fixing syntax

--- ruha.py
+++ ruha.py
@@ -1,14 +1,14 @@
 from gpiozero import Button
-from picamera2 import Picamera2
+from picamera2 import Picamera2, Preview
 from time import strftime
 
 button = Button(26)
 
 with Picamera2() as camera:
-    camera.resolution = (4056, 3040)
-    camera.framerate = 5
-    
-    camera.start_preview()
+    camera_config = camera.create_preview_configuration()
+    camera.configure(camera_config)
+    camera.start_preview(Preview.QTGL)
+    camera.start()
     while True:
         button.wait_for_press()
         filename = strftime("%Y%m%d-%H%M%S") + '.png'

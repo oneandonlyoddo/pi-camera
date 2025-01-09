1adff294111dd645ad7a44b4b83078973ff13c10
SILVER\jonas <jonas.otto@field.io>
2025-01-09 16:22:33 +0100

    reversing button

--- ruha.py
+++ ruha.py
@@ -4,13 +4,17 @@ from time import strftime
 
 button = Button(26)
 
+def capture(cam):
+    filename = strftime("%Y%m%d-%H%M%S") + '.png'
+    cam.capture_file(filename, format="png", wait=None)
+    print(f"\rCaptured {filename} succesfully")
+
+
 with Picamera2() as camera:
     camera_config = camera.create_preview_configuration()
     camera.configure(camera_config)
     camera.start_preview(Preview.QTGL)
     camera.start()
     while True:
-        button.wait_for_press()
-        filename = strftime("%Y%m%d-%H%M%S") + '.png'
-        camera.capture_file(filename, format="png", wait=None)
-        print(f"\rCaptured {filename} succesfully")
+        button.when_pressed = capture
+        

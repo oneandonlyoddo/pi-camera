8fa1beeb218f2af1a2680a77d7f37ad8409084b1
SILVER\jonas <jonas.otto@field.io>
2025-03-28 16:07:54 +0100

    chaninging parameters. this should be a full screen preview

--- ruha.py
+++ ruha.py
@@ -6,19 +6,19 @@ button = Button(26)
 camera = Picamera2()
 
 def capture():
-    filename = strftime("%Y%m%d-%H%M%S") + '.png'
+    filename = "./out/" + strftime("%Y%m%d-%H%M%S") + '.png'
     camera.switch_mode_and_capture_file("still", filename, format="png", wait=None)
     print(f"\rCaptured {filename} succesfully")
 
-WIDTH = 800
-HEIGHT = 480
+WIDTH = 1360
+HEIGHT = 768
 camera.preview_configuration.size = (400, 240)
 camera.preview_configuration.format = "YUV420"
-camera.still_configuration.size = (800, 480)
+camera.still_configuration.size = (1600, 960)
 camera.still_configuration.enable_raw()
 camera.still_configuration.raw.size = camera.sensor_resolution
-
-camera.start("preview", show_preview=True)
+camera.start_preview(Preview.DRM, x=0, y=0, width=WIDTH, height=HEIGHT)
+camera.start()
 
 while True:
     button.when_pressed = capture

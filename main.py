d410851daf654448f32a9c83873745bae5c63e7f
SILVER\jonas <jonas.otto@field.io>
2025-03-29 00:26:03 +0100

    testing camera control with keyboard

--- main.py
+++ main.py
@@ -6,34 +6,54 @@ from time import strftime
 from pynput import keyboard
 camera = Picamera2()
 loop = True
+eTime = 10000
+aGain = 1.0
+controlNeedsUpdate = False
 
 def on_press(key):
-    if key == keyboard.Key.up:
+    if key == keyboard.Key.esc:
         global loop
         loop = False
         print(loop)
-    elif key == keyboard.Key.down:
+    elif key == keyboard.Key.s:
         filename = "./web/static/" + strftime("%Y%m%d-%H%M%S") + '.png'
         camera.switch_mode_and_capture_file("still", filename, format="png", wait=None)
         print(f"\rCaptured {filename} succesfully")
+    elif key == keyboard.Key.up:
+        eTime += 10
+        controlNeedsUpdate = True
+    elif key == keyboard.Key.down:
+        eTime -= 10
+        controlNeedsUpdate = True
+    elif key == keyboard.Key.left:
+        aGain -= 0.05
+        controlNeedsUpdate = True
+    elif key == keyboard.Key.right:
+        aGain += 0.05
+        controlNeedsUpdate = True
+
+   
 
 listener = keyboard.Listener(on_press=on_press)
 listener.start()
 
 WIDTH = 800
 HEIGHT = 480
-camera.preview_configuration.size = (WIDTH, HEIGHT)
+camera.preview_configuration.size = (400, 240)
 camera.preview_configuration.format = "YUV420"
 camera.still_configuration.size = (1600, 960)
 camera.still_configuration.enable_raw()
 camera.still_configuration.raw.size = camera.sensor_resolution
+camera.set_controls({"ExposureTime": eTime, "AnalogueGain": aGain})
 camera.start_preview(Preview.QTGL, x=0, y=0, width=WIDTH, height=HEIGHT)
 camera.start()
 
 while loop:
-    pass
+    if controlNeedsUpdate:
+        camera.set_controls({"ExposureTime": eTime, "AnalogueGain": aGain})
+        controlNeedsUpdate = False
 else:
-    cam.stop_preview()
-    cam.stop()
-    cam.close()
+    camera.stop_preview()
+    camera.stop()
+    camera.close()
     tcflush(stdin, TCIOFLUSH)
